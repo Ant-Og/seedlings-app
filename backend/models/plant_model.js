159ff3ccbd00522499bed72ac6b3a4b15c8ea06d
henrychow <113641610+henrow5@users.noreply.github.com>
2023-02-02 17:00:41 +0000

    Add plant scheme & plant seeds

--- backend/models/plant_model.js
+++ backend/models/plant_model.js
@@ -2,36 +2,154 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const plantSchema = new Schema(
-  {
-    img: { type: String },
-    use: { type: String },
-    latinName: { type: String },
-    family: { type: String },
-    lightTolered: { type: String },
-    heightAtPurchase: { type: Number }, // in cm
-    lightIdeal: { type: String },
-    widthAtPurchase: { type: Number }, // in cm
-    id: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    widthPotential: { type: Number }, // in cm
-    heightPotential: { type: Number }, // in cm
-    description: { type: String },
-    temperatureMax: { type: Number }, // in celsius
-    watering: { type: String },
-    commonName: { type: Array },
-    temperatureMin: { type: Number }, // in celsius
-    colorOfLeaf: { type: Array },
-    climate: { type: String },
+const plantSchema = new Schema({
+  Categories: {
+    type: 'String',
   },
-
-  {
-    timestamps: true,
-  }
-);
+  Disease: {
+    type: 'String',
+  },
+  Img: {
+    type: 'String',
+  },
+  Use: {
+    type: ['String'],
+  },
+  'Latin name': {
+    type: 'String',
+  },
+  Insects: {
+    type: ['String'],
+  },
+  Avaibility: {
+    type: 'String',
+  },
+  Style: {
+    type: 'String',
+  },
+  Bearing: {
+    type: 'String',
+  },
+  'Light tolered': {
+    type: 'String',
+  },
+  'Height at purchase': {
+    M: {
+      type: 'Number',
+    },
+    CM: {
+      type: 'Number',
+    },
+  },
+  'Light ideal': {
+    type: 'String',
+  },
+  'Width at purchase': {
+    M: {
+      type: 'Number',
+    },
+    CM: {
+      type: 'Number',
+    },
+  },
+  id: {
+    type: 'String',
+  },
+  Appeal: {
+    type: 'String',
+  },
+  Perfume: {
+    type: 'Mixed',
+  },
+  Growth: {
+    type: 'String',
+  },
+  'Width potential': {
+    M: {
+      type: 'Number',
+    },
+    CM: {
+      type: 'Number',
+    },
+  },
+  'Common name (fr.)': {
+    type: 'String',
+  },
+  Pruning: {
+    type: 'String',
+  },
+  Family: {
+    type: 'String',
+  },
+  'Height potential': {
+    M: {
+      type: 'Number',
+    },
+    CM: {
+      type: 'Number',
+    },
+  },
+  Origin: {
+    type: ['String'],
+  },
+  Description: {
+    type: 'Mixed',
+  },
+  'Temperature max': {
+    F: {
+      type: 'Number',
+    },
+    C: {
+      type: 'Number',
+    },
+  },
+  'Blooming season': {
+    type: 'String',
+  },
+  Url: {
+    type: 'String',
+  },
+  'Color of leaf': {
+    type: ['String'],
+  },
+  Watering: {
+    type: 'String',
+  },
+  'Color of blooms': {
+    type: 'String',
+  },
+  Zone: {
+    type: ['String'],
+  },
+  'Common name': {
+    type: ['String'],
+  },
+  'Available sizes (Pot)': {
+    type: 'String',
+  },
+  'Other names': {
+    type: 'Mixed',
+  },
+  'Temperature min': {
+    F: {
+      type: 'Number',
+    },
+    C: {
+      type: 'Number',
+    },
+  },
+  'Pot diameter (cm)': {
+    M: {
+      type: 'Number',
+    },
+    CM: {
+      type: 'Number',
+    },
+  },
+  Climat: {
+    type: 'String',
+  },
+});
 
 const Plant = mongoose.model('Plant', plantSchema);
 
